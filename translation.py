88e6dfcc6fe38998d0b359ed39b4e49b4a9c2a3d
SANJU PAPPACHAN <sanjupappachan8943@gmail.com>
2020-05-20 09:01:02 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -48,7 +48,7 @@ Expires on: Untill my death """
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
    
-<b>👉 Create own Bot :</b> 👉 <a href="https://github.com/Sanju8943/LinkToFile">Diploy</a>
+<b>👉 Create own Bot :</b> 👉 <a href="https://t.me/joinchat/AAAAAE8Mr9SpgHSg2xoc8Q">Diploy</a>
 
 --------
 Send /me to know current plan details
